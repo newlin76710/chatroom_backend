b8d0b255236d759f89b4f362a18497536cef22b7
newlin76710 <newlin76710@gmail.com>
2025-12-09 20:07:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -261,5 +261,5 @@ function startAIAutoTalk(room) {
 }
 
 // --- Server ---
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 server.listen(port, () => console.log(`Server running on port ${port}`));
