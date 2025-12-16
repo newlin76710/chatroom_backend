cf4e1b0779124e2142fa17067cb8768e8d828247
newlin76710 <newlin76710@gmail.com>
2025-12-16 19:19:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,8 @@ if (!fs.existsSync(uploadDir)) {
 
 app.use("/songs", express.static(uploadDir));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // ---------- Helper ----------
 async function createGuest() {
