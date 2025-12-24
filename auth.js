6d0e8a689ad19f442ff17d42686f51dff2fdde5e
newlin76710 <newlin76710@gmail.com>
2025-12-24 17:22:26 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 import { pool } from "./db.js";
 
 export const authRouter = express.Router();
-export const ioTokens = {};
+export const ioTokens = new Map();
 // 訪客登入
 authRouter.post("/guest", async (req, res) => {
   try {
